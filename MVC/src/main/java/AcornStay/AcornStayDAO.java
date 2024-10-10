9e9163d349401afdf9f21c354d81b2cbd3180109
gedhanz <gedhanz69@gmail.com>
2024-10-10 09:36:48 +0900

    2024-10-10

---
+++ MVC/src/main/java/AcornStay/AcornStayDAO.java
@@ -0,0 +1,36 @@
+package AcornStay;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class AcornStayDAO {
+	
+	String driver = "oracle.jdbc.driver.OracleDriver" ;
+	String url="jdbc:oracle:thin:@localhost:1521:testdb";
+	String user="scott";
+	String password="tiger";
+	
+	
+	private Connection dbCon() {		
+		Connection con = null;
+		try {
+			Class.forName(driver);
+			con =DriverManager.getConnection(url, user, password);
+			if( con != null) { System.out.println("db ok");}
+			
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return con;
+	}
+	
+	
+	
+}
